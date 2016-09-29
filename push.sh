9500eb7df31384c9c7cd7e87a41a0c254a6afdc1
ip4368 <kg4368@yahoo.com.hk>
2016-09-29 16:39:58 +1000

    As this is not an official image, stackbrew is not needed, instead added a automated push script

--- push.sh
+++ push.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-hash git 2>/dev/null || { echo >&2 "git not found, exiting."; }
+repo='ip4368/node-armhf';
 
 array_4_6='4 argon';
 array_6_7='6 latest';
@@ -17,24 +17,20 @@ url='https://github.com/ip4368/docker-node'
 # sort version numbers with highest first
 IFS=$'\n'; versions=( $(echo "${versions[*]}" | sort -r) ); unset IFS
 
-# get the most recent commit which modified any of "$@"
-fileCommit() {
-	git log -1 --format='format:%H' HEAD -- "$@"
-}
-
-echo "# this file is generated via ${url}/blob/$(fileCommit "$self")/$self"
-echo
 echo "Maintainers: The Node.js Docker Team <${url}> (@nodejs)"
 echo "Forked by: ip4368"
 echo "Reason: provide armhf support"
 echo "GitRepo: ${url}.git"
 echo
 
-# prints "$2$1$3$1...$N"
-join() {
-	local sep="$1"; shift
-	local out; printf -v out "${sep//%/%%}%s" "$@"
-	echo "${out#$sep}"
+# push all version aliases
+push() {
+	local version;
+	for version in "$@"
+	do
+		echo "Pushing $repo:$version"
+		docker push $repo:$version
+	done
 }
 
 for version in "${versions[@]}"; do
@@ -42,14 +38,11 @@ for version in "${versions[@]}"; do
 	[ -f "$version/Dockerfile" ] || continue
 	
 	eval stub=$(echo "$version" | awk -F. '{ print "$array_" $1 "_" $2 }');
-	commit="$(fileCommit "$version")"
 	fullVersion="$(grep -m1 'ENV NODE_VERSION ' "$version/Dockerfile" | cut -d' ' -f3)"
 
 	versionAliases=( $fullVersion $version ${stub} )
-	
-	echo "Tags: $(join ', ' "${versionAliases[@]}")"
-	echo "GitCommit: ${commit}"
-	echo "Directory: ${version}"
+
+	push ${versionAliases[@]}
 	echo
 
 	variants=$(echo $version/*/ | xargs -n1 basename)
@@ -57,15 +50,11 @@ for version in "${versions[@]}"; do
 		# Skip non-docker directories
 		[ -f "$version/$variant/Dockerfile" ] || continue
 		
-		commit="$(fileCommit "$version/$variant")"
-		
 		slash='/'
 		variantAliases=( "${versionAliases[@]/%/-${variant//$slash/-}}" )
 		variantAliases=( "${variantAliases[@]//latest-/}" )
 
-		echo "Tags: $(join ', ' "${variantAliases[@]}")"
-		echo "GitCommit: ${commit}"
-		echo "Directory: ${version}/${variant}"
+		push ${variantAliases[@]}
 		echo
 	done
 done
